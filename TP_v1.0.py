e8b5bb20870de86fc65ef7a81e4716135950997f
Zeshooter <romainsoulie1973@gmail.com>
2022-01-05 16:34:15 +0100

    Gestion des erreurs + adherents illimités

--- TP_v1.0.py
+++ TP_v1.0.py
@@ -9,15 +9,27 @@ categorie=["Poussin","Cadet","Junior","Semi-Pro","Pro"]
 categorie_adherents=[]
 adherents=[]
 
-nbre_adherent=int(input("Combien d'adhérent voulez vous saisir ?\n"))
+saisie=False
+while saisie==False:
+    try:
+        nbre_adherent=int(input("Combien d'adhérent voulez vous saisir ?\n"))
+        saisie=True
+    except ValueError:
+        print("Vous devez entrer un nombre entier ?")
 
 for i in range(nbre_adherent):
     nom_adherents.append(input("Nom de l'adhérent ?\n"))
     prenom_adherents.append(input("prenom de l'adherent?\n"))
 
     # Vérification de l'admission de l'adhérent
+    saisie=False
+    while saisie==False:
+        try:
+            anneenaissance=int(input("Veuillez Indiquer l'année de naissance de l'adhérent ?\n"))
+            saisie=True
+        except ValueError:
+            print("Vous devez entrér l'année en chiffre")
 
-    anneenaissance=int(input("Veuillez Indiquer l'année de naissance de l'adhérent ?\n"))
     age_adherent=fonctions.age_adherent(anneenaissance)
 
     if age_adherent >6 and age_adherent <12:
@@ -43,13 +55,13 @@ for i in range(nbre_adherent):
 
     # Enregistrement de l'adhérent dans une liste + création de l'email
 
-    if nom_adherents:
-        adherents.append((nom_adherents[0],prenom_adherents[0],categorie_adherents[0],(prenom_adherents[0][0])+"."+(nom_adherents[0])+"@baton-rouge.fr"))
-        nom_adherents.pop(0)
-        prenom_adherents.pop(0)
-        categorie_adherents.pop(0)
-    else:
-        print("Cette personne ne peut pas adhérer au club de Quidditch car ici on discrimine les jeunes et les vieux !!!")
+if nom_adherents:
+    adherents.append((nom_adherents[0],prenom_adherents[0],categorie_adherents[0],(prenom_adherents[0][0])+"."+(nom_adherents[0])+"@baton-rouge.fr"))
+    nom_adherents.pop(0)
+    prenom_adherents.pop(0)
+    categorie_adherents.pop(0)
+else:
+    print("Cette personne ne peut pas adhérer au club de Quidditch car ici on discrimine les jeunes et les vieux !!!")
 
 
 
