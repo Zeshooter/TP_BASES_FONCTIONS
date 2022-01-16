25da5b9462bb87b908bce8c880919923a7734a4a
Zeshooter <romainsoulie1973@gmail.com>
2022-01-16 12:37:57 +0100

    Dedoublonnage OK

--- mix_csv.py
+++ mix_csv.py
@@ -6,7 +6,6 @@ adherent=[]
 
 # recherche des fichier csv
 repertoire=os.listdir("C:/Users/Ulrich 2/Documents/Adrar/TP_BASES_FONCTIONS/TP_BASES_FONCTIONS/")
-print(repertoire)
 
 # lecture des tuples dans les fichiers csv et stockage dans la liste adherent
 for i in repertoire:
@@ -15,7 +14,16 @@ for i in repertoire:
             lire=csv.reader(fichier_csv)
             for ligne in lire:
                 adherent.append(ligne)
-                print (adherent)
+
+
+# dedoublonnage des tuple
+new_list=[]
+for i in adherent:
+    if i not in new_list:
+        new_list.append(i)
+print (new_list)
+
+
 
 # ecriture des tuples dans un fichier
 with open("C:/Users/Ulrich 2/Documents/Adrar/TP_BASES_FONCTIONS/TP_BASES_FONCTIONS/adherents/inscrits_total.csv", "a") as fichier_csv:
@@ -24,4 +32,11 @@ with open("C:/Users/Ulrich 2/Documents/Adrar/TP_BASES_FONCTIONS/TP_BASES_FONCTIO
         writer.writerow(row)
 print("Un nouveau fichier a été crée avec tous les adhérents")
 
+
+# old_list=[]
+# with open ("C:/Users/Ulrich 2/Documents/Adrar/TP_BASES_FONCTIONS/TP_BASES_FONCTIONS/adherents/inscrits_total.csv", "r") as fichier_csv:
+#     reader=csv.reader(fichier_csv, delimiter=";", lineterminator="\n")
+#     for ligne in fichier_csv:
+#         old_list.append(ligne)
+# print(old_list)
 
