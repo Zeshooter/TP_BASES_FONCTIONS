900db51cd6f933b17535c7d6cc6debf29f305035
Zeshooter <romainsoulie1973@gmail.com>
2022-01-16 10:40:56 +0100

    v4.0

--- mix_csv.py
+++ mix_csv.py
@@ -3,8 +3,12 @@ import os
 
 adherent=[]
 # os.mkdir("adherents")
+
+# recherche des fichier csv
 repertoire=os.listdir("C:/Users/Ulrich 2/Documents/Adrar/TP_BASES_FONCTIONS/TP_BASES_FONCTIONS/")
 print(repertoire)
+
+# lecture des tuples dans les fichiers csv et stockage dans la liste adherent
 for i in repertoire:
     if ".csv" in i:
         with open(i,"r") as fichier_csv:
@@ -13,12 +17,11 @@ for i in repertoire:
                 adherent.append(ligne)
                 print (adherent)
 
-with open("C:/Users/Ulrich 2/Documents/Adrar/TP_BASES_FONCTIONS/TP_BASES_FONCTIONS/adherents/adherents.csv", "a") as fichier_csv:
+# ecriture des tuples dans un fichier
+with open("C:/Users/Ulrich 2/Documents/Adrar/TP_BASES_FONCTIONS/TP_BASES_FONCTIONS/adherents/inscrits_total.csv", "a") as fichier_csv:
     writer=csv.writer(fichier_csv, delimiter=";", lineterminator="\n")
     for row in adherent:
         writer.writerow(row)
+print("Un nouveau fichier a été crée avec tous les adhérents")
 
-# with open("inscrits"+*+"-"+*+"-"+*+".csv","r") as file :
-#     lecture=csv.reader(file)
-#     for row in lecture:
-#         print(row)
+
